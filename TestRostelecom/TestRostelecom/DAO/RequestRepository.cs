c330fcd642a7f8a3893e12f2576f780a7696dab8
Rodion <mar.rodion@gmail.com>
2016-02-18 23:33:22 +0300

    add request dao and FK in bd

---
+++ TestRostelecom/TestRostelecom/DAO/RequestRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRostelecom.DAO
+{
+    class RequestRepository
+    {
+        const string SQL_GET_REQUEST_BY_ID = 
+    }
+}
