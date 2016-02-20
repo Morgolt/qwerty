234a2f8b06dabb82cd879356774c39a21cea6874
Vladislav Suratov <suratovvlad@gmail.com>
2016-02-20 16:29:04 +0300

    Update translations

--- TestRostelecom/TestRostelecom/MainWindow.Designer.cs
+++ TestRostelecom/TestRostelecom/MainWindow.Designer.cs
@@ -40,6 +40,7 @@
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.requestsBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.clientsDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.requestDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.closeDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -49,7 +50,6 @@
             this.Masters = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dateOfDepartureDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.commentDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.requestsBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.menuStrip.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.requestsBindingSource)).BeginInit();
@@ -68,7 +68,7 @@
             this.menuStrip.Location = new System.Drawing.Point(0, 0);
             this.menuStrip.Name = "menuStrip";
             this.menuStrip.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.menuStrip.Size = new System.Drawing.Size(220, 24);
+            this.menuStrip.Size = new System.Drawing.Size(180, 24);
             this.menuStrip.TabIndex = 0;
             this.menuStrip.Text = "menuStrip1";
             // 
@@ -79,25 +79,25 @@
             this.toolStripSeparator1,
             this.exitToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
-            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.fileToolStripMenuItem.Text = "Файл";
             // 
             // exportToExcelToolStripMenuItem
             // 
             this.exportToExcelToolStripMenuItem.Name = "exportToExcelToolStripMenuItem";
-            this.exportToExcelToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
-            this.exportToExcelToolStripMenuItem.Text = "Export to Excel";
+            this.exportToExcelToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.exportToExcelToolStripMenuItem.Text = "Экспорт в Excel";
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(147, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(154, 6);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
-            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.exitToolStripMenuItem.Text = "Выход";
             // 
             // editToolStripMenuItem
             // 
@@ -105,34 +105,34 @@
             this.addRequestToolStripMenuItem,
             this.chooseDateFrameToolStripMenuItem});
             this.editToolStripMenuItem.Name = "editToolStripMenuItem";
-            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
-            this.editToolStripMenuItem.Text = "Edit";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.editToolStripMenuItem.Text = "Правка";
             // 
             // addRequestToolStripMenuItem
             // 
             this.addRequestToolStripMenuItem.Name = "addRequestToolStripMenuItem";
-            this.addRequestToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
-            this.addRequestToolStripMenuItem.Text = "Add Request";
+            this.addRequestToolStripMenuItem.Size = new System.Drawing.Size(241, 22);
+            this.addRequestToolStripMenuItem.Text = "Добавить заявку";
             // 
             // chooseDateFrameToolStripMenuItem
             // 
             this.chooseDateFrameToolStripMenuItem.Name = "chooseDateFrameToolStripMenuItem";
-            this.chooseDateFrameToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
-            this.chooseDateFrameToolStripMenuItem.Text = "Choose Date Frame";
+            this.chooseDateFrameToolStripMenuItem.Size = new System.Drawing.Size(241, 22);
+            this.chooseDateFrameToolStripMenuItem.Text = "Выбрать календарный период";
             // 
             // helpToolStripMenuItem
             // 
             this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.aboutToolStripMenuItem});
             this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
-            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
-            this.helpToolStripMenuItem.Text = "Help";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.helpToolStripMenuItem.Text = "Справка";
             // 
             // aboutToolStripMenuItem
             // 
             this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
-            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.aboutToolStripMenuItem.Text = "О приложении";
             // 
             // dataGridView1
             // 
@@ -160,72 +160,74 @@
             this.dataGridView1.Size = new System.Drawing.Size(984, 517);
             this.dataGridView1.TabIndex = 1;
             // 
+            // requestsBindingSource
+            // 
+            this.requestsBindingSource.DataSource = typeof(TestRostelecom.DAO.Requests);
+            // 
             // clientsDataGridViewTextBoxColumn
             // 
             this.clientsDataGridViewTextBoxColumn.DataPropertyName = "Clients";
-            this.clientsDataGridViewTextBoxColumn.HeaderText = "Clients";
+            this.clientsDataGridViewTextBoxColumn.HeaderText = "ФИО Клиента";
             this.clientsDataGridViewTextBoxColumn.Name = "clientsDataGridViewTextBoxColumn";
             this.clientsDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // requestDateDataGridViewTextBoxColumn
             // 
             this.requestDateDataGridViewTextBoxColumn.DataPropertyName = "RequestDate";
-            this.requestDateDataGridViewTextBoxColumn.HeaderText = "RequestDate";
+            this.requestDateDataGridViewTextBoxColumn.HeaderText = "Дата заявки";
             this.requestDateDataGridViewTextBoxColumn.Name = "requestDateDataGridViewTextBoxColumn";
             this.requestDateDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // closeDateDataGridViewTextBoxColumn
             // 
             this.closeDateDataGridViewTextBoxColumn.DataPropertyName = "CloseDate";
-            this.closeDateDataGridViewTextBoxColumn.HeaderText = "CloseDate";
+            this.closeDateDataGridViewTextBoxColumn.HeaderText = "Дата закрытия заявки";
             this.closeDateDataGridViewTextBoxColumn.Name = "closeDateDataGridViewTextBoxColumn";
             this.closeDateDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // addressDataGridViewTextBoxColumn
             // 
             this.addressDataGridViewTextBoxColumn.DataPropertyName = "Address";
-            this.addressDataGridViewTextBoxColumn.HeaderText = "Address";
+            this.addressDataGridViewTextBoxColumn.HeaderText = "Адрес";
             this.addressDataGridViewTextBoxColumn.Name = "addressDataGridViewTextBoxColumn";
             this.addressDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // servicesDataGridViewTextBoxColumn
             // 
             this.servicesDataGridViewTextBoxColumn.DataPropertyName = "Services";
-            this.servicesDataGridViewTextBoxColumn.HeaderText = "Services";
+            this.servicesDataGridViewTextBoxColumn.HeaderText = "Подключаемая услуга";
             this.servicesDataGridViewTextBoxColumn.Name = "servicesDataGridViewTextBoxColumn";
             this.servicesDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // operatorsDataGridViewTextBoxColumn
             // 
             this.operatorsDataGridViewTextBoxColumn.DataPropertyName = "Operators";
-            this.operatorsDataGridViewTextBoxColumn.HeaderText = "Operators";
+            this.operatorsDataGridViewTextBoxColumn.HeaderText = "ФИО Оператора";
             this.operatorsDataGridViewTextBoxColumn.Name = "operatorsDataGridViewTextBoxColumn";
             this.operatorsDataGridViewTextBoxColumn.ReadOnly = true;
+            this.operatorsDataGridViewTextBoxColumn.ToolTipText = "Оператор, принявший заявку";
             // 
             // Masters
             // 
             this.Masters.DataPropertyName = "Masters";
-            this.Masters.HeaderText = "Masters";
+            this.Masters.HeaderText = "ФИО Мастера";
             this.Masters.Name = "Masters";
             this.Masters.ReadOnly = true;
             // 
             // dateOfDepartureDataGridViewTextBoxColumn
             // 
             this.dateOfDepartureDataGridViewTextBoxColumn.DataPropertyName = "DateOfDeparture";
-            this.dateOfDepartureDataGridViewTextBoxColumn.HeaderText = "DateOfDeparture";
+            this.dateOfDepartureDataGridViewTextBoxColumn.HeaderText = "Дата выезда";
             this.dateOfDepartureDataGridViewTextBoxColumn.Name = "dateOfDepartureDataGridViewTextBoxColumn";
             this.dateOfDepartureDataGridViewTextBoxColumn.ReadOnly = true;
+            this.dateOfDepartureDataGridViewTextBoxColumn.ToolTipText = "Планируемая дата выезда мастера";
             // 
             // commentDataGridViewTextBoxColumn
             // 
             this.commentDataGridViewTextBoxColumn.DataPropertyName = "Comment";
-            this.commentDataGridViewTextBoxColumn.HeaderText = "Comment";
+            this.commentDataGridViewTextBoxColumn.HeaderText = "Комментарий";
             this.commentDataGridViewTextBoxColumn.Name = "commentDataGridViewTextBoxColumn";
             this.commentDataGridViewTextBoxColumn.ReadOnly = true;
-            // 
-            // requestsBindingSource
-            // 
-            this.requestsBindingSource.DataSource = typeof(TestRostelecom.DAO.Requests);
             // 
             // MainForm
             // 
